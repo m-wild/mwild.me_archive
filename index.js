a911b8ef791ca6f1f96706c9d3ce44261c0e8feb
Michael Wildman <michael@mwild.me>
2016-12-29 00:11:09 +1300

    metalsmith pluign to convert less to css

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const Metalsmith = require('metalsmith');
 const layouts    = require('metalsmith-layouts');
-const less       = require('metalsmith-less');
+const less       = require('less');
 
 Metalsmith(__dirname)
     .source('./src')
     .destination('./build')
-    .use(less())
+    //.clean(false)
+    .use(metalsmithLess)
     .use(layouts({engine: 'handlebars'}))
     .build(function(err) {
         if (err) throw err;
     });
+
+
+
+// convert all .less files to .css
+function metalsmithLess(files, metalsmith, done) {
+    return Promise.all(
+        Object.keys(files)
+        .filter(file => file.endsWith('.less'))
+        .map(file => 
+            less.render(files[file].contents.toString())
+                .then(cb => {
+                    files[file.replace('\.less', '.css')] = {
+                        contents: new Buffer(cb.css)
+                    };
+                })
+                .then(() => {
+                    delete files[file];
+                })
+    ))
+    .catch(err => done(err))
+    .then(() => done(null));
+}
