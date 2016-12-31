96850fb18a19286f50cb2411938e1717fdc6df2c
Michael Wildman <michael@mwild.me>
2017-01-01 11:22:07 +1300

    make metalsmith-less a module

--- index.js
+++ index.js
@@ -1,35 +1,19 @@
 const Metalsmith = require('metalsmith');
 const layouts    = require('metalsmith-layouts');
-const less       = require('less');
+const watch      = require('metalsmith-watch');
+const serve      = require('metalsmith-serve');
+const less       = require('./lib/metalsmith-less');
 
 Metalsmith(__dirname)
     .source('./src')
     .destination('./build')
-    //.clean(false)
-    .use(metalsmithLess)
+    .use(less())
     .use(layouts({engine: 'handlebars'}))
+    .use(serve())
+    .use(watch())
     .build(function(err) {
         if (err) throw err;
     });
 
 
 
-// convert all .less files to .css
-function metalsmithLess(files, metalsmith, done) {
-    return Promise.all(
-        Object.keys(files)
-        .filter(file => file.endsWith('.less'))
-        .map(file => 
-            less.render(files[file].contents.toString())
-                .then(cb => {
-                    files[file.replace('\.less', '.css')] = {
-                        contents: new Buffer(cb.css)
-                    };
-                })
-                .then(() => {
-                    delete files[file];
-                })
-    ))
-    .catch(err => done(err))
-    .then(() => done(null));
-}
