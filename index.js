1cf471eb517127a10c8cd0ccb7dcb8ebe530b4ea
Michael Wildman <michael@mwild.me>
2017-01-01 14:56:36 +1300

    start using pug instead of html

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const Metalsmith = require('metalsmith');
-const layouts    = require('metalsmith-layouts');
-const express    = require('metalsmith-express');
-const watch      = require('metalsmith-watch');
-const less       = require('./lib/metalsmith-less');
+const layouts   = require('metalsmith-layouts');
+const express   = require('metalsmith-express');
+const watch     = require('metalsmith-watch');
+const less      = require('./lib/metalsmith-less');
+const pug       = require('metalsmith-pug');
 
 Metalsmith(__dirname)
     .source('./src')
     .destination('./build')
     .use(less())
+    .use(pug())
     .use(layouts({engine: 'handlebars'}))
     .use(express())
     .use(watch({livereload:true}))
