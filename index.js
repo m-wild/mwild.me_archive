cbcd228b0120b772b90745d404d3593c52a60d0e
Michael Wildman <michael@mwild.me>
2017-01-04 20:43:39 +1300

    metalsmith build script

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
-const Metalsmith = require('metalsmith');
-//const layout
+const Metalsmith  = require('metalsmith');
+const collections = require('metalsmith-collections');
+const dates       = require('metalsmith-date-in-filename');
+const express     = require('metalsmith-express');
+const layouts     = require('metalsmith-layouts');
+const less        = require('metalsmith-less');
+const markdown    = require('metalsmith-markdown');
+const metallic    = require('metalsmith-metallic');
+const permalinks  = require('metalsmith-permalinks');
+const watch       = require('metalsmith-watch');
 
-var config = {
-  "title": "mwild",
-  "baseurl": "/blog",
-  "url": "http://mwild.me",
-  "permalink": "/:year/:title/",
-  "images": "https://static.mwild.me/images"
-};
 
 Metalsmith(__dirname)
+  .metadata({
+    site: {
+      title: 'mwild',
+      baseurl: '/blog',
+      url: 'http://mwild.me',
+      images: 'https://static.mwild.me/images'
+    }
+  })
   .source("./src")
   .destination("./build")
+  .use(less())
+  .use(dates())
+  .use(collections({
+    posts: {
+      pattern: '**/posts/**/*.md',
+      sortBy: 'date',
+      reverse: true
+    }
+  }))
+  .use(metallic())
+  .use(markdown({
+    gfm: true,
+    smartypants: true
+  }))
+  .use(permalinks({ // must be after markdown
+    pattern: ':date/:title',
+    date: 'YYYY'
+  }))
+  .use(layouts({
+    engine: 'pug'
+  }))
+  .use(express())
+  .use(watch({livereload:true}))
   .build(function(err) {
     if (err) throw err;
   });
