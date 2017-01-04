2bccc357ff978da46f78620c8e6b674a3b874dac
Michael Wildman <michael@mwild.me>
2017-01-05 00:11:26 +1300

    'added confirmation to publish script'

--- publish.ps1
+++ publish.ps1
@@ -1,16 +1,30 @@
 process {
 
+    function Get-Confirmation ([string] $prompt) {
+        $confirm = Read-Host -Prompt $prompt
+        if ($confirm -eq 'n') { exit 1 }
+    }
+
+
+    Get-Confirmation 'Have you built the site first? (Y/n)'
+    
     $commit_message = Read-Host -Prompt 'Commit message'
 
     git add --all
     git commit -m "$commit_message"
     git pull
     git push
-    cd .\build
+
+    Get-Confirmation 'Source synchronized.. continue to publish ./build? (Y/n)'
+    
+    Push-Location
+    Set-Location .\build
     git add --all
     git commit -m "$commit_message"
     git pull
     git push
-    cd ..
+    
+    Pop-Location
 
+    Write-Host 'Done' -ForegroundColor DarkCyan
 }
