bbeeb34410e63b57261dc5543485ccdb39a663ea
Michael Wildman <michael@mwild.me>
2017-01-11 15:43:11 +1300

    add publish script, command line arg '--watch'

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Metalsmith  = require('metalsmith');
 const collections = require('metalsmith-collections');
 const dates       = require('metalsmith-date-in-filename');
 const express     = require('metalsmith-express');
+const msIf        = require('metalsmith-if');
 const layouts     = require('metalsmith-layouts');
 const less        = require('metalsmith-less');
 const markdown    = require('metalsmith-markdown');
@@ -9,6 +10,7 @@ const metallic    = require('metalsmith-metallic');
 const permalinks  = require('metalsmith-permalinks');
 const watch       = require('metalsmith-watch');
 
+const argv = require('minimist')(process.argv.slice(2));
 
 Metalsmith(__dirname)
   .metadata({
@@ -41,8 +43,10 @@ Metalsmith(__dirname)
   .use(layouts({
     engine: 'pug'
   }))
-  .use(express())
-  .use(watch({livereload:true}))
+  .use(msIf(argv.watch, 
+    express()))
+  .use(msIf(argv.watch, 
+    watch({livereload: argv.watch}))) // have to double barrel this..
   .build(function(err) {
     if (err) throw err;
   });
---
+++ publish.ps1
@@ -0,0 +1,30 @@
+process {
+
+    function Get-Confirmation ([string] $prompt) {
+        $confirm = Read-Host -Prompt $prompt
+        if ($confirm -eq 'n') { exit 1 }
+    }
+
+    Write-Host 'Building site..'
+    node index.js
+
+    $commit_message = Read-Host -Prompt 'Commit message'
+
+    git add --all
+    git commit -m "$commit_message"
+    git pull
+    git push
+
+    Get-Confirmation 'Source synchronized.. continue to publish ./build? (Y/n)'
+    
+    Push-Location
+    Set-Location .\build
+    git add --all
+    git commit -m "$commit_message"
+    git pull
+    git push
+    
+    Pop-Location
+
+    Write-Host 'Done' -ForegroundColor DarkCyan
+}
