afcf904ec8ddcac40e119f60db49ef10b19273e5
Michael Wildman <michael@mwild.me>
2017-01-18 19:53:45 +1300

    add --watch command line arg

--- index.js
+++ index.js
@@ -4,16 +4,20 @@ const watch     = require('metalsmith-watch');
 const less      = require('metalsmith-less');
 const pug       = require('metalsmith-pug');
 
+const argv = require('minimist')(process.argv.slice(2));
+
 Metalsmith(__dirname)
-    .source('./src')
-    .destination('./build')
-    .use(less())
-    .use(pug({pretty:true}))
-    .use(express())
-    .use(watch({livereload:true}))
-    .build(function(err) {
-        if (err) throw err;
-    });
+  .source('./src')
+  .destination('./build')
+  .use(less())
+  .use(pug({pretty:true}))
+  .use(msIf(argv.watch, 
+    express()))
+  .use(msIf(argv.watch, 
+    watch({livereload: argv.watch}))) // have to double barrel this..
+  .build(function(err) {
+      if (err) throw err;
+  });
 
 
 
