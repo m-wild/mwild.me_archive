21bc491012d4bdb3c8fb792abffedad7276185de
Michael Wildman <michael@mwild.me>
2017-02-12 22:39:18 +1300

    move blog into website file structure

--- blog/README.md
+++
@@ -1,8 +0,0 @@
-#### tehmantra/blog
-
-[![Build Status](https://travis-ci.org/tehmantra/blog.svg?branch=master)](https://travis-ci.org/tehmantra/blog)
-
-##### Deployment
-Deployment is managed automagically by [travis-ci](https://travis-ci.org/tehmantra/blog).
-
-The site can also be manually deployed by force-pushing the output of build/ to gh-pages
--- index.js
+++ index.js
@@ -1,30 +1,70 @@
-const Metalsmith = require('metalsmith');
-const Autoprefix = require('less-plugin-autoprefix');
-const express   = require('metalsmith-express');
-const msIf      = require('metalsmith-if');
-const watch     = require('metalsmith-watch');
-const less      = require('metalsmith-lesser');
-const pug       = require('metalsmith-pug');
+const Metalsmith  = require('metalsmith');
+const Autoprefix  = require('less-plugin-autoprefix');
+const collections = require('metalsmith-collections');
+const dates       = require('metalsmith-date-in-filename');
+const disqus      = require('metalsmith-disqus');
+const express     = require('metalsmith-express');
+const msIf        = require('metalsmith-if');
+const layouts     = require('metalsmith-layouts');
+const less        = require('metalsmith-lesser');
+const markdown    = require('metalsmith-markdown');
+const metallic    = require('metalsmith-metallic');
+const permalinks  = require('metalsmith-permalinks');
+const watch       = require('metalsmith-watch');
 
 const argv = require('minimist')(process.argv.slice(2));
 
 Metalsmith(__dirname)
+  .metadata({
+    site: {
+      title: 'mwild',
+      url: 'https://mwild.me'
+    }
+  })
   .source('./src')
   .destination('./build')
   .use(less({
     copySource: false,
     lessOptions: {
-      plugins: [new Autoprefix({browsers: ["last 2 versions"]})]
+      plugins: [new Autoprefix({browsers: ['last 2 versions']})]
     }
   }))
-  .use(pug({pretty:true}))
-  .use(msIf(argv.watch, 
-    express()))
-  .use(msIf(argv.watch, 
-    watch({livereload: argv.watch}))) // have to double barrel this..
+  .use(dates())
+  .use(collections({
+    posts: {
+      pattern: '**/blog/posts/**/*.md',
+      sortBy: 'date',
+      reverse: true
+    }
+  }))
+  .use(metallic({ // highlight.js
+    languages: [] // disable lang autodetection, allows us to have 'plain text' code blocks
+  }))
+  .use(markdown({
+    gfm: true,
+    smartypants: true
+  }))
+  .use(permalinks({ // must be after markdown
+    pattern: ':title'
+  }))
+  .use(layouts({
+    engine: 'pug'
+  }))
+  .use(disqus({
+    siteurl: 'https://mwild.me/blog/',
+    shortname: 'mwild'
+  }))
+  // --watch
+  .use(msIf(argv.watch, express({
+    document_root: './build' 
+  })))
+  .use(msIf(argv.watch, watch({
+      livereload: argv.watch,
+      paths: {
+        '${source}/css/**/*': '**/*', // css and layouts trigger a full rebuild
+        './layouts/**/*': '**/*',
+        '${source}/**/*': true // everything else rebuilds itself
+      }})))
   .build(function(err) {
-      if (err) throw err;
+    if (err) throw err;
   });
-
-
-
