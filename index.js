bea922daa201306dcd1c8486ba76bc264f0cb84a
Michael Wildman <michael@mwild.me>
2020-01-25 10:25:43 +1300

    Replaced metalsmith(if,serve,watch) with light-server

--- index.js
+++ index.js
@@ -1,70 +1,41 @@
 const Metalsmith  = require('metalsmith');
-const Autoprefix  = require('less-plugin-autoprefix');
 const collections = require('metalsmith-collections');
 const dates       = require('metalsmith-date-in-filename');
-const express     = require('metalsmith-express');
-const msIf        = require('metalsmith-if');
 const layouts     = require('metalsmith-layouts');
-const less        = require('metalsmith-lesser');
 const markdown    = require('metalsmith-markdown');
 const metallic    = require('metalsmith-metallic');
 const permalinks  = require('metalsmith-permalinks');
-const pug         = require('metalsmith-pug');
-const watch       = require('metalsmith-watch');
-
-// Parse command line args
-const argv = require('minimist')(process.argv.slice(2));
+const sass        = require('metalsmith-sass');
 
 Metalsmith(__dirname)
-  .metadata({
+  .metadata({               // metadata variables are available in layout files
     site: {
       title: 'mwild'
     }
   })
-  .clean(true)
-  .source('./src')
-  .destination('./build')
-
-  // 1. --- do non-blog related stuff
-  .use(less({
-    copySource: false,
-    lessOptions: {
-      plugins: [new Autoprefix()]
-  }}))
-  .use(pug()) // pug should be before markdown (because we use layouts{engine:pug}, md gets converted to pug)
-
-  // 2. --- then blog stuff
-  .use(dates())
-  .use(collections({
-    posts: {
+  .source('./src')          // source directory
+  .destination('./build')   // destination directory
+  .clean(true)              // clean the destination directory before building
+  .use(sass())              // transpile sass to css
+  .use(dates())             // add dates to post metadata based on filenames
+  .use(collections({        
+    posts: {                // create a collection called 'posts' of every markdown file in the blog/posts folder
       pattern: '**/blog/posts/**/*.md',
-      sortBy: 'date',
-      reverse: true
+      sortBy: 'date',       // order posts by dates from the filename
+      reverse: true         // order in reverse
   }}))
-  .use(metallic({ // highlight.js -- must be before markdown -> needs to see the md ```
-    languages: [] // disable lang autodetection, allows us to have 'plain text' code blocks
+  .use(metallic({           // highlight.js -- must be before markdown as it needs to see the md back-ticks: ```
+    languages: []           // disable language auto-detection, allows us to have 'plain text' code blocks
   }))
-  .use(markdown({
-    gfm: true,
-    smartypants: true
+  .use(markdown({           // convert markdown to HTML
+    gfm: true,              // using github-flavoured-markdown
+    smartypants: true       // converts ascii dashes, quotes etc, to their html equivalents
   }))
-  .use(permalinks({ // must be after markdown -> only works on .html
-    pattern: 'blog/:title',
-    relative: false // we use absolute paths for everything instead of duplicating files
+  .use(permalinks({         // change file paths to permanent URLs. must be after markdown as it only works on .html files
+    pattern: 'blog/:title', // paths based on the page title. e.g. /blog/some-post
+    relative: false         // prevent duplicating sibling files (e.g. css)
   }))
-  .use(layouts())
-
-  // 3. --- then additions to blog posts
-
-  // 4. --- watch
-  .use(msIf(argv.watch, express()))
-  .use(msIf(argv.watch, watch({
-    livereload: argv.watch,
-    paths: {
-      '${source}/**/*': true, // each file reloads itself
-      './layouts/**/*': '**/*' // layouts need to reload everything, which unfortunately breaks collections
-  }})))
-  // 5. --- finally, build the site
-  .build(function(err) {
+  .use(layouts())           // wrap layouts around html
+  .build(function(err) {    // build the site!
     if (err) throw err;
   });
